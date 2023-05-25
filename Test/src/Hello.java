7dc30ede033a0fb0d61d8569b81940a2fcbc8422
jungwookjin <134483553+jungwookjin@users.noreply.github.com>
2023-05-25 10:33:34 +0900

    Update Hello.java

--- Test/src/Hello.java
+++ Test/src/Hello.java
@@ -1,14 +1,14 @@
 public class Hello {
 	public static void main(String[] args) {
 		// 로컬에서 최종 수정했습니다.111
-		// 원격에서 다시 수정했음.
+		// 원격에서 다시 최종 수정했음.
 		System.out.println("Hello world");
 		
 		System.out.println("변경");
 		
 		System.out.println("세번째 커밋");
 		
-		//주석 111
+		//주석 222
 		
 	}
 	
